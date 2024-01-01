2d9fe643645038a1ccc327a821c1d20c6b296800
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-01-01 13:51:53 +0100

    refractor Pyrogram method to support BOT API type

--- pyrogram/methods/messages/set_message_reaction.py
+++ pyrogram/methods/messages/set_message_reaction.py
@@ -20,35 +20,38 @@ from typing import Union
 from typing import List
 
 import pyrogram
-from pyrogram import raw
+from pyrogram import raw, types
 
 
-class SendReaction:
-    async def send_reaction(
+class SetMessageReaction:
+    async def set_message_reaction(
         self: "pyrogram.Client",
         chat_id: Union[int, str],
         message_id: int = None,
-        emoji: Union[int, str, List[Union[int, str]]] = None,
-        big: bool = False
+        reaction: List[types.ReactionType] = [],
+        is_big: bool = False
     ) -> bool:
-        """Send a reaction to a message.
+        """Use this method to change the chosen reactions on a message.
+        Service messages can't be reacted to.
+        Automatically forwarded messages from
+        a channel to its discussion group have the
+        same available reactions as messages in the channel.
 
-        .. include:: /_includes/usable-by/users.rst
+        .. include:: /_includes/usable-by/users-bots.rst
 
         Parameters:
             chat_id (``int`` | ``str``):
                 Unique identifier (int) or username (str) of the target chat.
 
             message_id (``int``):
-                Identifier of the message.
+                Identifier of the target message. If the message belongs to a media group, the reaction is set to the first non-deleted message in the group instead.
 
-            emoji (``int`` | ``str`` | List of ``int`` | ``str``, *optional*):
-                Reaction emoji.
+            emoji (List of :obj:`~pyrogram.types.ReactionType` *optional*):
+                New list of reaction types to set on the message.
                 Pass None as emoji (default) to retract the reaction.
-                Pass list of int or str to react multiple emojis.
 
             big (``bool``, *optional*):
-                Pass True to show a bigger and longer reaction.
+                Pass True to set the reaction with a big animation.
                 Defaults to False.
 
         Returns:
@@ -57,34 +60,23 @@ class SendReaction:
         Example:
             .. code-block:: python
 
-                # Send a reaction
-                await app.send_reaction(chat_id, message_id=message_id, emoji="🔥")
-
-                # Send a multiple reactions
-                await app.send_reaction(chat_id, message_id=message_id, emoji=["🔥", "❤️"])
-
                 # Retract a reaction
                 await app.send_reaction(chat_id, message_id=message_id)
         """
-        if isinstance(emoji, list):
-            emoji = [
-                raw.types.ReactionCustomEmoji(document_id=i)
-                if isinstance(i, int)
-                else raw.types.ReactionEmoji(emoticon=i)
-                for i in emoji
-            ]
-        else:
-            emoji = [raw.types.ReactionEmoji(emoticon=emoji)] if emoji else []
-
         if message_id is not None:
-            await self.invoke(
+            r = await self.invoke(
                 raw.functions.messages.SendReaction(
                     peer=await self.resolve_peer(chat_id),
                     msg_id=message_id,
-                    reaction=emoji,
-                    big=big
+                    reaction=[
+                        r.write(self)
+                        for r in reaction
+                    ],
+                    big=is_big
                 )
             )
+            for i in r.updates:
+                if isinstance(i, raw.types.UpdateMessageReactions):
+                    return types.MessageReactions._parse(self, i.reactions)
         else:
             raise ValueError("You need to pass one of message_id!")
-        return True
