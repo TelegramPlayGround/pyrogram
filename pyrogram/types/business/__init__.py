da049b450c66b701b83980d8b41cb731ea88c7fe
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-06-06 11:43:12 +0200

    Message
    successful_payment
    reply_invoice

--- pyrogram/types/business/__init__.py
+++ pyrogram/types/business/__init__.py
@@ -23,6 +23,10 @@ from .business_opening_hours import BusinessOpeningHours
 from .business_opening_hours_interval import BusinessOpeningHoursInterval
 from .collectible_item_info import CollectibleItemInfo
 from .invoice import Invoice
+from .labeled_price import LabeledPrice
+from .order_info import OrderInfo
+from .shipping_address import ShippingAddress
+from .successful_payment import SuccessfulPayment
 
 __all__ = [
     "BusinessConnection",
@@ -32,4 +36,8 @@ __all__ = [
     "BusinessOpeningHoursInterval",
     "CollectibleItemInfo",
     "Invoice",
+    "LabeledPrice",
+    "OrderInfo",
+    "ShippingAddress",
+    "SuccessfulPayment",
 ]
